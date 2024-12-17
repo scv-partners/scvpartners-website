5a24286c622e8edaf81880507415d2d10640361d
Michael Kang <michael3dk@gmail.com>
2024-12-17 11:46:32 -0800

    change the menu structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#F26419',
+        'secondary': '#55DDE0',
+        'accent': '#F6AE2D',
+      },
+    },
+  },
+  plugins: [],
 }
