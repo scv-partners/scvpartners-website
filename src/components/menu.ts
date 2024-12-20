04eb447ea3179298c7e6027c32e6171d4da1fd7a
Michael Kang <michael3dk@gmail.com>
2024-12-20 08:00:12 -0800

    Set up the screen for the mobile devices (#6)

---
+++ src/components/menu.ts
@@ -0,0 +1,24 @@
+export const menu: Record<string, string> = {
+  company: '회사소개',
+  business: '의결권대행',
+  customer: '고객지원',
+} as const
+
+export const submenu = {
+  company: [
+    { name: '인사말', link: '/company/greetings' },
+    { name: '회사개요', link: '/company/outline' },
+    { name: '사업소개', link: '/company/area' },
+  ],
+  business: [
+    { name: '자사강점', link: '/business/strength' },
+    { name: '업무절차', link: '/business/process' },
+    { name: '수행사례', link: '/business/cases' },
+    { name: '주요분쟁사례', link: '/business/disputes' },
+    { name: '기타수행사례', link: '/business/success' },
+  ],
+  customer: [
+    { name: '오시는길', link: '/customer/directions' },
+    { name: '공지사항', link: '/customer/notice' },
+  ],
+} as const
